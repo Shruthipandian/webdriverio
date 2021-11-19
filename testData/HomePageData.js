a7588d982cf8990d5e7eeafe1992bc8a1fdb976c
Shruthi pandian <shruthi.pandian.99@gmail.com>
2021-11-19 01:12:52 +0100

    **

--- testData/HomePageData.js
+++ testData/HomePageData.js
@@ -3,9 +3,7 @@ class HomePageData {
         this.pageTitles = {
             homePageTitle: 'A million more | Volvo Cars - International',
             carConfiguratorPage: "Design your Volvo | Volvo Cars International",
-            homepagevideo: 'https://youtu.be/fj_WFwVOYn8 | Volvo Cars International',
-            learnmore_btn: 'A heritage of safety innovations | Volvo Cars International'
-
+           
         }
     }
 
